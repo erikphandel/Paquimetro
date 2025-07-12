748713ce2df3462e9a6f4a0d8d6e98cc3f37608a
Erik Handel <erikphandel@gmail.com>
2025-07-11 21:48:20 -0300

    Corrigido bug visual, ajuste para padrão monetário brasileiro

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const mensagem = document.getElementById('mensagem')
 class Paquimetro {
 
     constructor (valorInserido) {
-        this.valorInserido = parseFloat(document.getElementById('valor').value);
+        this.valorInserido = parseFloat(document.getElementById('valor').value.replace(',','.'));
         }
 
     calcularTempo() {
@@ -12,13 +12,13 @@ class Paquimetro {
                 mensagem.textContent =  "Valor insuficiente, favor digitar um valor acima de R$1,00";
             } else if (this.valorInserido >=1 && this.valorInserido<1.75) {
                 valorTroco = this.valorInserido - 1;
-                mensagem.textContent = "Você pode estacionar por 30 minutos. Seu troco é: R$" + valorTroco;
+                mensagem.textContent = "Você pode estacionar por 30 minutos. Seu troco é: R$" + valorTroco.toFixed(2);
             } else if (this.valorInserido >=1.75 && this.valorInserido<3) {
                 valorTroco = this.valorInserido - 1.75;
-                mensagem.textContent = "Você pode estacionar por 1 hora. Seu troco é: R$" + valorTroco;
+                mensagem.textContent = "Você pode estacionar por 1 hora. Seu troco é: R$" + valorTroco.toFixed(2);
             } else {
                 valorTroco = this.valorInserido - 3;
-                mensagem.textContent = "Você pode estacionar por 2 horas. Seu troco é: R$" + valorTroco;
+                mensagem.textContent = "Você pode estacionar por 2 horas. Seu troco é: R$" + valorTroco.toFixed(2);
             };
     }
 }
