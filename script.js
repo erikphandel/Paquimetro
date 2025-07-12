8bbebb8979210e859f2c9a2d172d4a0aaeb0d0b0
Erik Handel <erikphandel@gmail.com>
2025-07-11 21:38:32 -0300

    Versão funcional

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-function mensagem() {
-    document.getElementById('mensagem')
-}
+const mensagem = document.getElementById('mensagem')
 
-class paquimetro {
+class Paquimetro {
+
     constructor (valorInserido) {
         this.valorInserido = parseFloat(document.getElementById('valor').value);
         }
+
     calcularTempo() {
         let valorTroco;
-            if (valorInserido <0) {
+            if (this.valorInserido <1) {
                 mensagem.textContent =  "Valor insuficiente, favor digitar um valor acima de R$1,00";
-                return;
-            } else if (this.valorInserido >=1 || this.valorInserido<1.75) {
+            } else if (this.valorInserido >=1 && this.valorInserido<1.75) {
                 valorTroco = this.valorInserido - 1;
                 mensagem.textContent = "Você pode estacionar por 30 minutos. Seu troco é: R$" + valorTroco;
-            } else if (this.valorInserido >=1.75||this.valorInserido<3) {
+            } else if (this.valorInserido >=1.75 && this.valorInserido<3) {
                 valorTroco = this.valorInserido - 1.75;
                 mensagem.textContent = "Você pode estacionar por 1 hora. Seu troco é: R$" + valorTroco;
             } else {
                 valorTroco = this.valorInserido - 3;
-                mensagem.textContent = "Você pode estacionar por 2 horas. Seu troco é: R$" + valorTroco
+                mensagem.textContent = "Você pode estacionar por 2 horas. Seu troco é: R$" + valorTroco;
             };
     }
+}
+
+function calcular() {
+    const paquimetro = new Paquimetro();
+    paquimetro.calcularTempo()
 }
